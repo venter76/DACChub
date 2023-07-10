44f34dcdb7292e9cfbe41ad729f1fe2fc1f3e731
Your Name <you@example.com>
2023-07-10 13:52:31 +0200

    5

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 require('dotenv').config();
+const crypto = require('crypto');
 
   
 
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
+const bcrypt = require('bcryptjs');
 
 
 const db_username = process.env.DB_USERNAME;
@@ -33,8 +35,89 @@ const connectDB = async () => {
   }
 };
 
+const infoSchema = new mongoose.Schema({
+  enter: String,
+  password: String,
+});
+
+const Info = new mongoose.model("Info", infoSchema);
+
+module.exports = Info;
+
+
+async function createOrUpdateInfo() {
+
+  const enter = process.env.ENTER;
+  const password = process.env.PASSWORD;
+
+  console.log('USERNAME:', process.env.ENTER);
+console.log('PASSWORD:', process.env.PASSWORD);
+
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Check if Info collection already has an entry
+  const info = await Info.findOne({});
+
+  if (info) {
+    // Info collection has an entry, update it
+    info.enter = enter;
+    info.password = hashedPassword;
+    await info.save();
+  } else {
+    // Info collection is empty, create a new entry
+    const newInfo = new Info({ enter, password: hashedPassword });
+    await newInfo.save();
+  }
+
+  console.log('Username and password have been saved to the database.');
+}
+
+// Run the function
+createOrUpdateInfo().catch(console.error);
+
+
+
 
 app.get('/', (req, res) => {
+  res.render('login', { message: '' });
+  
+});
+
+
+
+
+  app.post('/login', async (req, res) => {
+    try {
+      const { enter, password } = req.body;
+      
+      // Fetch the stored Info
+      const info = await Info.findOne({});
+  
+      if (!info) {
+        // No Info in database
+        return res.status(400).send('No information found in the database.');
+      }
+  
+      if (enter !== info.enter || !(await bcrypt.compare(password, info.password))) {
+        // Username or password is incorrect
+        return res.status(401).render('login', { message: 'Invalid username or password.' });
+      }
+  
+      // Username and password are correct
+      return res.status(200).render('home');
+    
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send('Internal server error.');
+      
+    }
+  });
+
+
+app.get('/home', (req, res) => {
     res.render('home');
   });
 
@@ -58,4 +141,10 @@ connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("listening for requests");
     })
-  })
+  });
+
+
+
+ 
+
+
