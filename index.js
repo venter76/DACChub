da2fede3cd217688621b444c415f2e8f12c29983
Your Name <you@example.com>
2023-07-11 10:40:19 +0200

    2

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ app.use(session({
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
       httpOnly: true, // prevents JavaScript from making changes
+      maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
   }));
 
